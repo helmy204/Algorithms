1f4209ee15a0a363a1ab5f2b2f1d0f31c856ffa5
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2019-03-24 16:08:59 +0300

    CountRepeatedWordInText

---
+++ Algorithms/Algorithms.CountRepeatedWordInText/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.CountRepeatedWordInText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "hello world";
+            string word = "hello";
+
+            int count = CountOccurences(text, word);
+
+            Console.WriteLine(count);
+        }
+
+        static int CountOccurences(string text, string word)
+        {
+            string[] textWords = text.Split(' ');
+            int count = 0;
+
+            foreach (string textWord in textWords)
+            {
+                if (word.Equals(textWord))
+                    count++;
+            }
+
+            return count;
+        }
+    }
+}
